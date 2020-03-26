a2d45257e36b3a2dff99813e088fcfc2ddb95297
ChangmoKang <dkssudckdah@gmail.com>
2020-03-27 00:48:03 +0900

    product를 사용하여 코드를 줄임

--- changmo/week3/행렬의_곱셈.py
+++ changmo/week3/행렬의_곱셈.py
@@ -1,11 +1,12 @@
+from itertools import product
+
 def solution(arr1, arr2):
     arr2 = list(map(list, zip(*arr2)))
     
     R, C = len(arr1), len(arr2)
 
-    answer = [[0]*C for _ in range(R)]    
-    for r in range(R):
-        for c in range(C):
-            answer[r][c] = sum(v1*v2 for v1, v2 in zip(arr1[r], arr2[c]))
+    answer = [[0]*C for _ in range(R)]
+    for r, c in product(range(R), range(C)):
+        answer[r][c] = sum(v1*v2 for v1, v2 in zip(arr1[r], arr2[c]))
     
     return answer
