24fdeea34d3e27e157bb2ec70a37b1261009ea58
eian <eianlee1124@gmail.com>
2020-03-31 08:05:05 +0900

    add 카드게임.py

--- Eian/week4/카드게임.py
+++ Eian/week4/카드게임.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python3
-
+    
 def solution(left, right):
     N = len(left)
     dp = [[0] * (N+1) for _ in range(N+1)]
+    
+    
     for i in range(N-1, -1, -1):
         for j in range(N-1, -1, -1):
             if left[i] > right[j]:
                 dp[i][j] = dp[i][j+1] + right[j]
             else:
                 dp[i][j] = max(dp[i+1][j], dp[i+1][j+1])
+            
     return dp[0][0]
-    
-    
+
+
 if __name__ == "__main__":
             
     print(solution([3,3,1], [7,7,1]))
