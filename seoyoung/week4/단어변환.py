e90544042b767c6f5e9c7b33c331499235b9335b
tjdud0123 <tjdud0123@naver.com>
2020-04-03 20:15:37 +0900

    Update 단어변환.py

--- seoyoung/week4/단어변환.py
+++ seoyoung/week4/단어변환.py
@@ -5,8 +5,10 @@ def can_change(cur_word, words):
     for word in words:
         diff = 0
         for x, y in zip(cur_word, word):
-            if x != y: diff += 1
-        if diff == 1: cand.append(word)
+            if x != y: 
+                diff += 1
+        if diff == 1: 
+            cand.append(word)
     return cand
 
 def solution(begin, target, words):
@@ -14,7 +16,8 @@ def solution(begin, target, words):
     que = deque([(begin, 0)])
     while que:
         cur_word, cur_count = que.popleft()
-        if cur_word == target: return cur_count
+        if cur_word == target: 
+            return cur_count
         for word in can_change(cur_word, words):
             if word not in visited:
                 que.append((word, cur_count + 1))
